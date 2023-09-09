fde65d97d15239f7a71490461e434633ae5236ff
Tessy Samuel <111059413+Vendysam@users.noreply.github.com>
2023-09-09 21:00:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ function updateDisplay() {
 // Update the display initially and every second
 updateDisplay();
 setInterval(updateDisplay, 1000);
+
+
+const dateElement = document.querySelector(".date");
+
+const date = new Date ( );
+const day = date.getDay( );
+
+const formatDate = ( ) =>{
+    return day ==1
+    ?  "Monday"
+    : day ==2
+    ?  "Tuesday"
+    : day == 3
+    ?  "Wednesday"
+    : day == 4
+    ?  "Thurday"
+    : day ==5
+    ?  "Friday"
+    : day == 6
+    ?  "Saturday"
+    : "Sunday";
+}; 
+
+dateElement.textContent = formatDate( );
+
+
+console.log(formatDate);
